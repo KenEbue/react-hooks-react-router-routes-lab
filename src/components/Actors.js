60932742ec992b0f0ddcbd476694cd9c0394b85c
Kenneth <kenneth.ebue@gmail.com>
2023-06-18 14:44:09 -0700

    Lel ID change

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor) => (
+        <div key={actor.name}> {/* Add a unique key prop */}
+          <h3>Name: {actor.name}</h3>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}> {/* Use index as key for movies list */}
+                {movie}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar"> {/* Add the "navbar" class to the div */}
+      <ul> {/* Add an unordered list */}
+        <li>
+          <NavLink to="/">Home</NavLink> {/* Wrap each NavLink in a list item */}
+        </li>
+        <li>
+          <NavLink to="/movies">Movies</NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors">Directors</NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors">Actors</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;
